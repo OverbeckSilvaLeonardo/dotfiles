dd832119a9617ac404efe20f86d631d271aa5826
Leonardo Overbeck da Silva <overbecksilva.leonardo@gmail.com>
2026-04-16 15:47:14 -0300

    update nvim config

--- nvim/lua/kickstart/lsp_servers.lua
+++ nvim/lua/kickstart/lsp_servers.lua
@@ -6,10 +6,11 @@ return {
     settings = {
       python = {
         analysis = {
+          diagnosticMode = 'workspace',
+          typeCheckingMode = 'basic',
           autoSearchPaths = true,
           useLibraryCodeForTypes = true,
-          diagnosticMode = 'workspace',
-          typeCheckingMode = 'standard',
+          autoImportCompletions = true,
           -- extraPaths can be added here if your django project has a non-standard layout
         },
       },
@@ -33,9 +34,7 @@ return {
     on_init = function(client)
       if client.workspace_folders then
         local path = client.workspace_folders[1].name
-        if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
-          return
-        end
+        if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
       end
 
       client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
