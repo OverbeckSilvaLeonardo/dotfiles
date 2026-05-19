6163e4134b4f05c8ade021250e1e8bf789ae9371
Leonardo Overbeck da Silva <overbecksilva.leonardo@gmail.com>
2026-05-19 11:15:36 -0300

    feat(nvim): show abs and relative line numbers on all lines
    statuscolumn renders sign, absolute (v:lnum), and relative (v:relnum)
    columns simultaneously — default hybrid mode hides one or the other.

--- nvim/init.lua
+++ nvim/init.lua
@@ -98,11 +98,9 @@ vim.g.have_nerd_font = false
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
 vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
 vim.o.relativenumber = true
+vim.o.statuscolumn = '%s %{v:lnum} %{v:relnum} '
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
