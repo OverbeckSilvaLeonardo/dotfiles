2221890984e383c3f7054d5c4668d320bc4aa9e2
Leonardo Overbeck da Silva <overbecksilva.leonardo@gmail.com>
2026-06-16 10:40:28 -0300

    add ts and vue support

--- nvim/lua/kickstart/lsp_servers.lua
+++ nvim/lua/kickstart/lsp_servers.lua
@@ -20,12 +20,28 @@ return {
     filetypes = { 'jinja', 'jinja2', 'html', 'htmldjango', 'django' },
   },
   -- rust_analyzer = {},
-  --
-  -- Some languages (like typescript) have entire language plugins that can be useful:
-  --    https://github.com/pmizio/typescript-tools.nvim
-  --
-  -- But for many setups, the LSP (`ts_ls`) will work just fine
-  -- ts_ls = {},
+
+  -- vtsls handles TS/JS; Vue plugin hooked in via @vue/typescript-plugin for .vue script blocks
+  vtsls = {
+    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+    settings = {
+      vtsls = {
+        tsserver = {
+          globalPlugins = {
+            {
+              name = '@vue/typescript-plugin',
+              location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+              languages = { 'vue' },
+              configNamespace = 'typescript',
+            },
+          },
+        },
+      },
+    },
+  },
+
+  -- vue_ls handles .vue SFC template/style; hybrid mode pairs with vtsls above
+  vue_ls = {},
 
   stylua = {}, -- Used to format Lua code
 
